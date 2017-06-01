233c3815eee861251c2189a850aff328522b1d7b
jean-1314 <isfiremsk@gmail.com>
2017-06-01 14:31:05 +0300

    Fix gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 var gulp = require("gulp");
 
-var sass = require("gulp-less");
+var less = require("gulp-less");
 var plumber = require("gulp-plumber");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
